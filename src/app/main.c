bf4d73ef761f401b48e75260dfae5e8103094949
Luisrit19 <lr6853@g.rit.edu>
2024-11-21 21:24:40 -0500

    changed main.c to old one

--- src/app/main.c
+++ src/app/main.c
@@ -1,64 +1,74 @@
 #include "main.h"
 
 #include <stdbool.h>
-#include <stdio.h>
 
 #include "can.h"
 #include "clock.h"
 #include "gpio.h"
 #include "i2c.h"
-#include "spi.h"
-#include "error_handler.h"
+#include "interrupts.h"
 
 #include "FreeRTOS.h"
-#include "queue.h"
 #include "task.h"
-
-#include <stm32g4xx_hal.h>
 
 
 void heartbeat_task(void *pvParameters) {
-    (void) pvParameters;
-    while(true) {
-        core_GPIO_toggle_heartbeat();
-        vTaskDelay(100 * portTICK_PERIOD_MS);
-    }
+	(void) pvParameters;
+	while(true)
+	{
+		CAN_send(0, 8, 0x0123456789abcdef);
+	}
 }
 
-int main(void) {
-    HAL_Init();
+int main(void)
+{
+	// Drivers
+	if (!Clock_init()) {
+		Error_Handler();
+	}
+	GPIO_init();
+	if (!I2C_init()) {
+		Error_Handler();
+	}
+	if (!CAN_init()) {
+		Error_Handler();
+	}
+	Interrupts_init();
 
-    // Drivers
-    core_heartbeat_init(GPIOB, GPIO_PIN_9);
-    core_GPIO_set_heartbeat(GPIO_PIN_RESET);
+	// App modules
+	//OutboardAccelerometers_init();
 
-    if (!core_clock_init()) error_handler();
+	for (int i = 0; i < 1000000; i++) {}
 
-    int err = xTaskCreate(heartbeat_task,
-        "heartbeat",
+	int err = xTaskCreate(heartbeat_task, 
+        "heartbeat", 
         1000,
         NULL,
         4,
         NULL);
     if (err != pdPASS) {
-        error_handler();
+        Error_Handler();
     }
-
-    NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
 
     // hand control over to FreeRTOS
     vTaskStartScheduler();
 
     // we should not get here ever
-    error_handler();
-    return 1;
+    Error_Handler();
 }
 
 // Called when stack overflows from rtos
 // Not needed in header, since included in FreeRTOS-Kernel/include/task.h
-void vApplicationStackOverflowHook( TaskHandle_t xTask, char *pcTaskName) {
-    (void) xTask;
-    (void) pcTaskName;
+void vApplicationStackOverflowHook( TaskHandle_t xTask, char *pcTaskName)
+{
+	(void) xTask;
+	(void) pcTaskName;
 
-    error_handler();
+    Error_Handler();
+}
+
+void Error_Handler(void)
+{
+	Interrupts_disable();
+	while (true) {}
 }
