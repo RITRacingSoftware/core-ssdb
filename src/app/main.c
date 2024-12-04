0460821f57d5d80a357b996f2db683e7e6d33e91
Luisrit19 <lr6853@g.rit.edu>
2024-12-03 21:23:32 -0500

    more driver copies and changes

--- src/app/main.c
+++ src/app/main.c
@@ -1,74 +1,126 @@
 #include "main.h"
 
 #include <stdbool.h>
+#include <stdio.h>
 
 #include "can.h"
+#include "FrontSSDB/driver_front.h"
+#include "RearSSDB/driver_rear.h"
+#include "ssdb_config.h"
 #include "clock.h"
 #include "gpio.h"
-#include "i2c.h"
-#include "interrupts.h"
+#include "error_handler.h"
+#include "formula_sensor_dbc.h"
 
-#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "queue.h"
 #include "task.h"
+
+#include <stm32g4xx_hal.h>
 
 
 void heartbeat_task(void *pvParameters) {
-	(void) pvParameters;
-	while(true)
-	{
-		CAN_send(0, 8, 0x0123456789abcdef);
-	}
+    (void) pvParameters;
+    while(true) {
+        core_GPIO_toggle_heartbeat();
+        vTaskDelay(100 * portTICK_PERIOD_MS);
+    }
 }
 
-int main(void)
-{
-	// Drivers
-	if (!Clock_init()) {
-		Error_Handler();
-	}
-	GPIO_init();
-	if (!I2C_init()) {
-		Error_Handler();
-	}
-	if (!CAN_init()) {
-		Error_Handler();
-	}
-	Interrupts_init();
+void collect_sensors_task(void *pvParameters) {
+    (void) pvParameters;
+    while (true) {
+#ifdef TARGET_REAR
+        SSDB_rear_collect_sensors();
+        vTaskDelay(SSDB_REAR_LOOP_DELAY * portTICK_PERIOD_MS);
+#endif
+#ifdef TARGET_FRONT
+        SSDB_front_collect_sensors();
+        vTaskDelay(SSDB_REAR_LOOP_DELAY * portTICK_PERIOD_MS);
+#endif
+    }
+}
 
-	// App modules
-	//OutboardAccelerometers_init();
+void transmit_sensor_task(void *pvParameters) {
+    (void) pvParameters;
+    CAN_sensor_transmit_task();
+    error_handler();
+}
 
-	for (int i = 0; i < 1000000; i++) {}
+void transmit_main_task(void *pvParameters) {
+    (void) pvParameters;
+    CAN_main_transmit_task();
+    error_handler();
+}
 
-	int err = xTaskCreate(heartbeat_task, 
-        "heartbeat", 
+int main(void) {
+    HAL_Init();
+
+    // Drivers
+    core_heartbeat_init(GPIOB, GPIO_PIN_15);
+    core_GPIO_set_heartbeat(GPIO_PIN_RESET);
+
+    if (!core_clock_init()) error_handler();
+    if (!CAN_init()) error_handler();
+#ifdef TARGET_REAR
+    if (!SSDB_rear_init()) error_handler();
+#endif
+#ifdef TARGET_FRONT
+    if (!SSDB_front_init()) error_handler();
+#endif
+
+    int err = xTaskCreate(heartbeat_task,
+        "heartbeat",
         1000,
         NULL,
         4,
         NULL);
     if (err != pdPASS) {
-        Error_Handler();
+        error_handler();
     }
+    err = xTaskCreate(collect_sensors_task,
+        "collect_sensors",
+        1000,
+        NULL,
+        4,
+        NULL);
+    if (err != pdPASS) {
+        error_handler();
+    }
+    err = xTaskCreate(transmit_sensor_task,
+        "tx_sensor",
+        1000,
+        NULL,
+        4,
+        NULL);
+    if (err != pdPASS) {
+        error_handler();
+    }
+    err = xTaskCreate(transmit_main_task,
+        "tx_main",
+        1000,
+        NULL,
+        4,
+        NULL);
+    if (err != pdPASS) {
+        error_handler();
+    }
+
+    NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
 
     // hand control over to FreeRTOS
     vTaskStartScheduler();
 
     // we should not get here ever
-    Error_Handler();
+    error_handler();
+    return 1;
 }
 
 // Called when stack overflows from rtos
 // Not needed in header, since included in FreeRTOS-Kernel/include/task.h
-void vApplicationStackOverflowHook( TaskHandle_t xTask, char *pcTaskName)
-{
-	(void) xTask;
-	(void) pcTaskName;
+void vApplicationStackOverflowHook( TaskHandle_t xTask, char *pcTaskName) {
+    (void) xTask;
+    (void) pcTaskName;
 
-    Error_Handler();
-}
-
-void Error_Handler(void)
-{
-	Interrupts_disable();
-	while (true) {}
+    error_handler();
 }
